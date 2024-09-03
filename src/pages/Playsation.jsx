607e483856bb4b067285cf604adf78abc5ffc06a
Fabio Silveira <fabio.eds@hotmail.com>
2024-09-02 20:13:20 -0700

    AddedCode

--- src/pages/Playsation.jsx
+++ src/pages/Playsation.jsx
@@ -2,12 +2,21 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { logo } from "../data/data";
+
 
 
 
 function Playsation() {
 
     const [product, setProduct] = useState(null)
+    const [count, setCount] = useState(0);
+    const [price, setPrice] = useState(0)
 
     useEffect(() => {
         async function fetchApi() {
@@ -23,19 +32,64 @@ function Playsation() {
     }, [])
 
 
+    const handleIncrement = () => {
+        if (count < 10) { // Condição: só incrementa se count for menor que 10
+            setCount(count + 1);
+        } else {
+            console.log("O valor máximo foi atingido!");
+        }
+    };
+
+    const handleDecrement = () => {
+        if (count > 0) { // Condição: só decrementar se count for maior que 0
+            setCount(count - 1);
+        } else {
+            console.log("O valor mínimo foi atingido!");
+        }
+    };
+
+    const handlePrice = () => {
+        if (price < 4.500) {
+            setPrice(count * 450.00);
+        } else {
+            console.log("The maximum price has reached")
+        }
+    }
+
     return (
         <>
-            <h1>{product === null ? "loading" : product.title}</h1>
-            <img src={product === null ? "loading" : product.thumbnail} alt="" />
-            <div className="btn-playstation">
-            <ButtonGroup aria-label="Basic example">
-                <Button variant="secondary">-</Button>
-                <Button variant="secondary">restart</Button>
-                <Button variant="secondary">+</Button>
-            </ButtonGroup>
-            <Button variant="outline-secondary">ADD TO CART</Button>
+ 
+            <Navbar bg="light" data-bs-theme="light">
+                <Container>
+                <img className="navbar" src={logo} alt="Bootstrap" width="85" height="60"></img>
+                    
+                    <Nav className="me-auto">
+                    
+                        <Nav.Link href="#features">Playstation</Nav.Link>
+                        <Nav.Link href="#pricing">Xbox</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <h4 className="h4-title">{product === null ? "loading" : product.title}</h4>
+            <img className="playstation-pic" src={product === null ? "loading" : product.thumbnail} alt="" />
+
+            <div className="input-price">
+                <InputGroup>
+                    <InputGroup.Text>$</InputGroup.Text>
+                    <InputGroup.Text>{price}</InputGroup.Text>
+                </InputGroup>
             </div>
-            
+
+            <div className="btn-playstation">
+                <ButtonGroup aria-label="Basic example">
+                    <Button onClick={handleDecrement} variant="secondary">-</Button>
+                    <Button disabled variant="secondary">{count}</Button>
+                    <Button onClick={handleIncrement} variant="secondary">+</Button>
+                </ButtonGroup>
+                <Button onClick={handlePrice} variant="outline-secondary">ADD TO CART</Button>
+
+            </div>
         </>
     )
 }
